959cad03c4547c3de6cfaa6f4ec1b513c243768d
Jie Lu <jielu@tuni.fi>
2024-12-09 20:34:31 +0200

    connect, add, and fetch data

---
+++ backend/database.js
@@ -0,0 +1,55 @@
+const { Pool } = require('sqlite3').verbose();
+const path = require('path');
+
+
+const db = new sqlite3.Database(path.resolve(__dirname, 'iot_barbecue.db'), (err) => {
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+        db.run(`
+            CREATE TABLE IF NOT EXISTS SensorData (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                temperature REAL NOT NULL,
+                sensorLocation TEXT NOT NULL,
+                timestamp TEXT DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+    }
+});
+
+//add sensor temperature and other data into database
+const insertSensorData = (data) => {
+    const { temperature, humidity, sensorLocation, timestamp } = data;
+    return new Promise((resolve, reject) => {
+        db.run(
+            `
+            INSERT INTO SensorData (temperature, humidity, sensorLocation, timestamp)
+            VALUES (?, ?, ?, ?)
+            `,
+            [temperature, humidity, sensorLocation, timestamp],
+            function (err) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve({ id: this.lastID });
+            }
+        );
+    });
+};
+
+// Fetch sensor data from the database 
+// Fetch all data because there is not so many types of data here, 
+//but in the future there could be humidity sensor etc.
+const fetchSensorData = () => {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM SensorData ORDER BY timestamp DESC', [], (err, rows) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+    });
+};
+
+module.exports = { insertSensorData, fetchSensorData };
